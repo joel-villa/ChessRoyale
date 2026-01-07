8e73351df630f9e27f460174d6f156037b4c7560
Joel Villarreal II <joel.villa2329@gmail.com>
2026-01-07 14:33:57 -0700

    the king himself

---
+++ src/GameLogic/Components/Pieces/King.java
@@ -0,0 +1,67 @@
+package GameLogic.Components.Pieces;
+
+import GameLogic.Components.Card;
+import GameLogic.Components.Piece;
+import Util.Coordinate;
+import Util.CoordinateFunctions;
+import Util.Pieces;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * The big boy himself
+ */
+public class King implements Piece {
+    private final int capacity = Pieces.KING.getCapacity();
+    private final int[][] movement = {
+            {0,0,0,0,0},
+            {0,1,1,1,0},
+            {0,1,0,1,0},
+            {0,1,1,1,0},
+            {0,0,0,0,0}
+    };
+    private List<Card> weapons;
+    private final int playerID;
+
+    public King(int playerID) {
+        this.playerID = playerID;
+    }
+
+    @Override
+    public Set<Coordinate> getMovement() {
+        //TODO: Implement this one differently
+        return CoordinateFunctions.bitArrayToCoord(movement);
+    }
+
+    @Override
+    public Set<Coordinate> getAttack() {
+        Set<Coordinate> attack = new HashSet<>();
+        for (Card c : weapons) {
+            attack.addAll(c.getAttack());
+        }
+        return attack;
+    }
+
+    @Override
+    public List<Card> getWeapons() {
+        return weapons;
+    }
+
+    @Override
+    public void takeDamage() {
+        weapons.remove(0);
+        //TODO: Move to discard?
+    }
+
+    @Override
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    @Override
+    public int getCapacity() {
+        return capacity;
+    }
+}
